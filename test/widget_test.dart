2428fb75130348c2dc1ca1037c92efd81a05b5cb
royfuwei <royfuwei@gamil.com>
2022-03-03 13:26:36 +0800

    [ci] add unit test v1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,4 +11,21 @@ import 'package:seeks_app_prototype/infrastructures/develop/develop_app.dart';
 
 // import 'package:seeks_app_prototype/main.dart';
 
-void main() {}
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(DevelopApp());
+
+    // Verify that our counter starts at 0.
+    // expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
+  });
+}
